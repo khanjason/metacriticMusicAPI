c733ed115d97e56f579df410ce0122e1231aa2c7
khanjason <jasonxdecember@gmail.com>
2024-11-10 17:00:36 +0000

    add client test

---
+++ test_client.py
@@ -0,0 +1,17 @@
+from sqlalchemy import null
+from client import *
+import pytest, json, datetime
+
+@pytest.mark.first
+def test_initialise():
+    client = AlbumReleasesClient()
+    pytest.client = client
+    assert pytest.client != null
+
+def test_fetch_anticipated_albums():
+    result = pytest.client.fetch_anticipated_album_releases()
+    assert len(result) != 0 
+
+def test_fetch_confirmed_albums():
+    result = pytest.client.fetch_confirmed_album_releases()
+    assert len(result) != 0 
